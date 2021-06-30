75cebd38587af7b163ba5225252b9a219370e476
Braian Henrique <80363931+Braianhenrike@users.noreply.github.com>
2021-06-30 16:48:14 -0300

    revision

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=f329bd0d4b596891f3f9ae62b68ea7cc'
 const trending = 'https://api.themoviedb.org/3/trending/movie/week?' +API_KEY
+const treiler = 'https://www.youtube.com/results?search_query='
 
 const buttonElement = document.querySelector('#search')
 const inputElement = document.querySelector('#inputValue')
@@ -43,7 +44,6 @@ function showMovies(data) {
     main.innerHTML = ''
       
 
-
     data.sort((a, b) => (a.vote_average > b.vote_average) ? -1 : 1)
 
     data.forEach(movie => {
@@ -56,11 +56,13 @@ function showMovies(data) {
           <div class="movie-info">
               <h3>${title}</h3>
               <span class="${getColor(vote_average)}">${vote_average}</span>
-              <p>${release_date}</p>
           </div>
           <div class="overview">
           <h3>Overview</h3>
           ${overview}
+          <h4>Release date:${release_date}</h4>
+          <br>
+          <a href=" ${treiler} + ${title}">Click to see the trailer</a>
       </div>
       
       
